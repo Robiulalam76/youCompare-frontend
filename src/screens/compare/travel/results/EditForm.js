f1a3f1f48cf016f8f7ecba369f5346410da769e6
Good Bloke <goodbloke@pop-os.localdomain>
2021-10-30 23:37:35 +0600

    update responsiveness travel/compare page

---
+++ src/screens/compare/travel/results/EditForm.js
@@ -0,0 +1,21 @@
+/**
+ * Travellers details edit form
+ */
+import React from 'react'
+
+// mui components
+import Typography from '@mui/material/Typography'
+
+import InputBox from '../../../../components/customStyledComponents/InputBox'
+
+function EditForm(props) {
+  return (
+    <div>
+      <Typography>Edit Form</Typography>
+      <InputBox label="">
+      </InputBox>
+    </div>
+  )
+}
+
+export default EditForm
